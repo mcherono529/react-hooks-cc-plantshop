1188179222f1835cd8688d54d13a894bd0d08e6d
mercy <mcherono529@gmail.com>
2024-11-11 11:32:25 +0300

    plant

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  const addPlant = (newPlant) => {
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlant),
+    })
+      .then((response) => response.json())
+      .then((data) => setPlants([...plants, data]));
+  };
+
+  const updatePlant = (updatedPlant) => {
+    setPlants(plants.map((plant) => (plant.id === updatedPlant.id ? updatedPlant : plant)));
+  };
+
+  const deletePlant = (id) => {
+    setPlants(plants.filter((plant) => plant.id !== id));
+  };
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={filteredPlants} onUpdatePlant={updatePlant} onDeletePlant={deletePlant} />
     </main>
   );
 }
